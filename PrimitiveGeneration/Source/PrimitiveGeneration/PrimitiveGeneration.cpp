d67dd1acfb0bdb808a683a88336a86a8733202ad
Sanctorum003 <lushuchengsky@126.com>
2020-01-06 20:07:50 +0800

    200106-[Primitive Generation]-第二卷-因为刚才的失误代码全部丢了，这次重新来，顺便全部同步到github上

---
+++ PrimitiveGeneration/Source/PrimitiveGeneration/PrimitiveGeneration.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PrimitiveGeneration.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PrimitiveGeneration, "PrimitiveGeneration" );
---
+++ PrimitiveGeneration/Source/PrimitiveGeneration/PrimitiveGenerationGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PrimitiveGenerationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PRIMITIVEGENERATION_API APrimitiveGenerationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
