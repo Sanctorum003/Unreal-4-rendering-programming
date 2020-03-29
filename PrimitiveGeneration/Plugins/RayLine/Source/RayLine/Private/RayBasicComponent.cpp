e22aa1dd23e7f78dc11bebf691b641c19146fff9
Sanctorum003 <lushuchengsky@126.com>
2020-03-29 15:06:56 +0800

    200329-[Primitive Generation]-使用自己程序化生成的模型 4.19版本代码，并尽可能增加注释

--- PrimitiveGeneration/Plugins/RayLine/Source/RayLine/Private/RayBasicComponent.cpp
+++ PrimitiveGeneration/Plugins/RayLine/Source/RayLine/Private/RayBasicComponent.cpp
@@ -14,35 +14,104 @@
 #include "DynamicMeshBuilder.h"
 #include "EngineGlobals.h"
 #include "Engine/Engine.h"
-
-#include "Runtime/Engine/Classes/Kismet/KismetSystemLibrary.h"
 #include "Private/AssetTypeActions/AssetTypeActions_StaticMesh.h"
 
-/** Index Buffer */
-class FRayLineMeshIndexBuffer : public FIndexBuffer
+/** 4.17版本 Vertex Buffer */
+/*
+class FRayLineMeshVertexBuffer : public FVertexBuffer
 {
 public:
 
 	virtual void InitRHI() override
 	{
 		FRHIResourceCreateInfo CreateInfo;
+		VertexBufferRHI = RHICreateVertexBuffer(NumVerts * sizeof(FDynamicMeshVertex), BUF_Dynamic, CreateInfo);
+	}
+
+	int32 NumVerts;
+};
+*/
+
+
+
+/** Index Buffer */
+// FRayLineMeshIndexBuffer将定义在SecneProxy中用于接受来自逻辑线程的数据
+// 4.17的Vertex Buffer跟Index Buffer类似
+class FRayLineMeshIndexBuffer : public FIndexBuffer
+{
+public:
+
+	//初始化此资源使用的RHI资源。 在进入资源和RHI都已初始化的状态时调用。仅由渲染线程调用。
+	virtual void InitRHI() override
+	{
+		FRHIResourceCreateInfo CreateInfo;
+		//FIndexBuffer::IndexBufferRHI
+		//IndexBufferRHI用于接受来自逻辑线程的Index数据,在渲染线程的场景代理中将index数据传给DrawPolicy
+		// Index数据块 ：步长 / 总大小 / 缓冲类型 / 创建信息
 		IndexBufferRHI = RHICreateIndexBuffer(sizeof(int32), NumIndices * sizeof(int32), BUF_Dynamic, CreateInfo);
 	}
 
 	int32 NumIndices;
 };
 
+/** 4.17版本 Vertex Factory */
+/*
+class FCustomMeshVertexFactory : public FLocalVertexFactory
+{
+public:
+
+	FCustomMeshVertexFactory()
+	{}
+
+	void Init(const FRayLineMeshVertexBuffer* VertexBuffer)
+	{
+		if (IsInRenderingThread())
+		{
+			// Initialize the vertex factory's stream components.
+			FDataType NewData;
+			NewData.PositionComponent = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer, FDynamicMeshVertex, Position, VET_Float3);
+			NewData.TextureCoordinates.Add(
+				FVertexStreamComponent(VertexBuffer, STRUCT_OFFSET(FDynamicMeshVertex, TextureCoordinate), sizeof(FDynamicMeshVertex), VET_Float2)
+			);
+			NewData.TangentBasisComponents[0] = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer, FDynamicMeshVertex, TangentX, VET_PackedNormal);
+			NewData.TangentBasisComponents[1] = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer, FDynamicMeshVertex, TangentZ, VET_PackedNormal);
+			SetData(NewData);
+		}
+		else
+		{
+			ENQUEUE_UNIQUE_RENDER_COMMAND_TWOPARAMETER(
+				InitCableVertexFactory,
+				FCustomMeshVertexFactory*, VertexFactory, this,
+				const FRayLineMeshVertexBuffer*, VertexBuffer, VertexBuffer,
+				{
+					// Initialize the vertex factory's stream components.
+					FDataType NewData;
+			NewData.PositionComponent = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer,FDynamicMeshVertex,Position,VET_Float3);
+			NewData.TextureCoordinates.Add(
+				FVertexStreamComponent(VertexBuffer,STRUCT_OFFSET(FDynamicMeshVertex,TextureCoordinate),sizeof(FDynamicMeshVertex),VET_Float2)
+			);
+			NewData.TangentBasisComponents[0] = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer,FDynamicMeshVertex,TangentX,VET_PackedNormal);
+			NewData.TangentBasisComponents[1] = STRUCTMEMBER_VERTEXSTREAMCOMPONENT(VertexBuffer,FDynamicMeshVertex,TangentZ,VET_PackedNormal);
+			VertexFactory->SetData(NewData);
+				});
+		}
+	}
+};
+*/
+
+//FRayLineDynamicData结构体，把它视作一个数据包，方便我们从逻辑层把数据们打包一起发送到渲染线程
+//在SendRenderDynamicData_Concurrent中将逻辑线程中的数据打包发送到渲染线程
 struct FRayLineDynamicData
 {
-	TArray<FVector> HitpointsPosition;
+	TArray<FVector> HitPointsPosition;
 	//You can also define some other data to send
 };
 
 /** Scene proxy */
+// 渲染线程部分
 class FRayLineMeshSceneProxy : public FPrimitiveSceneProxy
 {
 public:
-
 	SIZE_T GetTypeHash() const override
 	{
 		static size_t UniquePointer;
@@ -51,17 +120,30 @@ public:
 
 	FRayLineMeshSceneProxy(URayBasicComponent* Component)
 		: FPrimitiveSceneProxy(Component)
+		, Material(NULL)
+		, VertexFactory(GetScene().GetFeatureLevel(),"FRayLineScenceProxy") // VertexFactory需要用特征等级初始化.
+		, DynamicData(NULL)
 		, MaterialRelevance(Component->GetMaterialRelevance(GetScene().GetFeatureLevel()))
-		, VertexFactory(GetScene().GetFeatureLevel(), "FRayLineMeshSceneProxy")
 	{
 
+		//VertexBuffer.NumVerts = GetRequiredVertexCount(); 4.17
+
+		//4.19
+		//InitWithDummyData()函数将VertexFactory与VertexBuffers绑定
 		VertexBuffers.InitWithDummyData(&VertexFactory, GetRequiredVertexCount());
+
+		
 		IndexBuffer.NumIndices = GetRequiredIndexCount();
 
-		const FColor VertexColor(255, 255, 255);
+		// 4.17 Init vertex factory
+		// VertexFactory.Init(&VertexBuffer);
 
-		// Enqueue initialization of render resource
+		// 4.17 Enqueue initialization of render resource
+		//BeginInitResource(&VertexBuffer);
+
+		// 调用过程 BeginInitResource(&Resource) -> Resource->InitResource() -> InitDynamicRHI(); && InitRHI();
 		BeginInitResource(&IndexBuffer);
+		//BeginInitResource(&VertexFactory);
 
 		// Grab material
 		Material = Component->GetMaterial(0);
@@ -72,38 +154,85 @@ public:
 
 	}
 
-	void BuildMesh(FRayLineDynamicData* NewDynamicData)
+	virtual ~FRayLineMeshSceneProxy()
 	{
+		VertexBuffers.PositionVertexBuffer.ReleaseResource();
+		VertexBuffers.StaticMeshVertexBuffer.ReleaseResource();
+		VertexBuffers.ColorVertexBuffer.ReleaseResource();
+		
+		//VertexBuffer.ReleaseResource(); 4.17
+		IndexBuffer.ReleaseResource();
+		VertexFactory.ReleaseResource();
 
-		TArray<FDynamicMeshVertex> Vertices;
-		TArray<int32> Indices;
-		for (int32 i = 0; i < NewDynamicData->HitpointsPosition.Num(); i++)
+		if (DynamicData != NULL)
+			delete DynamicData;
+	}
+
+	int32 GetRequiredVertexCount() const
+	{
+		return 40;
+	}
+
+	int32 GetRequiredIndexCount() const
+	{
+		return 60;
+	}
+
+	//根据NewDynamicData->HitPointsPosition中的点，构建Mesh的顶点和Index顺序
+	void BuildRayLineMesh(const TArray<FVector>& Points,TArray<FDynamicMeshVertex>& Vertices,TArray<int32>& Indices)
+	{
+		
+		for (int32 i = 0; i < Points.Num(); i++)
 		{
 			FDynamicMeshVertex newvert0;
-			newvert0.Position = NewDynamicData->HitpointsPosition[i] + FVector(0, 100, 0);
-			newvert0.TextureCoordinate[0] = FVector2D(i / 10, 1);
-
+			newvert0.Position = Points[i] + FVector(-100, 100, 0);
 			FDynamicMeshVertex newvert1;
-			newvert1.Position = NewDynamicData->HitpointsPosition[i] + FVector(0, -100, 0);
-			newvert1.TextureCoordinate[0] = FVector2D(i / 10, 0);
+			newvert1.Position = Points[i] + FVector(100, 100, 0);
+			FDynamicMeshVertex newvert2;
+			newvert2.Position = Points[i] + FVector(-100, -100, 0);
+			FDynamicMeshVertex newvert3;
+			newvert3.Position = Points[i] + FVector(100, -100, 0);
 
 			Vertices.Add(newvert0);
 			Vertices.Add(newvert1);
-		}
-		for (int32 i = 0; i < NewDynamicData->HitpointsPosition.Num() - 1; i++)
-		{
-			Indices.Add(2 * i);
-			Indices.Add(2 * i + 2);
-			Indices.Add(2 * i + 1);
-			Indices.Add(2 * i + 2);
-			Indices.Add(2 * i + 3);
-			Indices.Add(2 * i + 1);
-		}
+			Vertices.Add(newvert2);
+			Vertices.Add(newvert3);
 
+			Indices.Add(4 * i);
+			Indices.Add(4 * i + 1);
+			Indices.Add(4 * i + 2);
+			Indices.Add(4 * i + 1);
+			Indices.Add(4 * i + 3);
+			Indices.Add(4 * i + 2);
+		}
+	}
+
+	//因为我们这个是动态的模型，所以需要有一个函数负责在渲染线程接收逻辑层发送过来的数据
+	//在SendRenderDynamicData_Concurrent中调用，这个函数在 逻辑线程中
+	/** Called on render thread to assign new dynamic data */
+	void SetDynamicData_RenderThread(FRayLineDynamicData* NewDynamicData)
+	{
+		check(IsInRenderingThread());
+
+		// Free existing data if present
+		if (DynamicData)
+		{
+			delete DynamicData;
+			DynamicData = NULL;
+		}
+		DynamicData = NewDynamicData;
+
+		//构建RayLine Mesh的顶点和index
+		TArray<FDynamicMeshVertex> Vertices;
+		TArray<int32> Indices;
+		
+		BuildRayLineMesh(NewDynamicData->HitPointsPosition,Vertices,Indices);
+
+		//确认顶点和index的数量
 		check(Vertices.Num() == GetRequiredVertexCount());
 		check(Indices.Num() == GetRequiredIndexCount());
 
-
+		//将Vertices写入VertexBuffers中
 		for (int i = 0; i < Vertices.Num(); i++)
 		{
 			const FDynamicMeshVertex& Vertex = Vertices[i];
@@ -114,6 +243,7 @@ public:
 			VertexBuffers.ColorVertexBuffer.VertexColor(i) = Vertex.Color;
 		}
 
+		//将VertexBuffers和IndexBufferData的数据通关内存复制的方法写入RHI中对应的位置，提供给系统API使用（win下就是提供给dx使用的数据）
 		{
 			auto& VertexBuffer = VertexBuffers.PositionVertexBuffer;
 			void* VertexBufferData = RHILockVertexBuffer(VertexBuffer.VertexBufferRHI, 0, VertexBuffer.GetNumVertices() * VertexBuffer.GetStride(), RLM_WriteOnly);
@@ -147,38 +277,26 @@ public:
 		RHIUnlockIndexBuffer(IndexBuffer.IndexBufferRHI);
 	}
 
-	/** Called on render thread to assign new dynamic data */
-	void SetDynamicData_RenderThread(FRayLineDynamicData* NewDynamicData)
-	{
-		check(IsInRenderingThread());
 
-		BuildMesh(NewDynamicData);
-
-	}
-
-	virtual ~FRayLineMeshSceneProxy()
-	{
-		VertexBuffers.PositionVertexBuffer.ReleaseResource();
-		VertexBuffers.StaticMeshVertexBuffer.ReleaseResource();
-		VertexBuffers.ColorVertexBuffer.ReleaseResource();
-		IndexBuffer.ReleaseResource();
-		VertexFactory.ReleaseResource();
-	}
-
-	int32 GetRequiredVertexCount() const
-	{
-		return 20;
-	}
-
-	int32 GetRequiredIndexCount() const
-	{
-		return 54;
-	}
-
+	//这个函数负责把模型数据加入到绘制队列Collector中
+	/**
+	* Gathers the primitive's dynamic mesh elements.  This will only be called if GetViewRelevance declares dynamic relevance. //该操作在GetViewRelevance中
+	* This is called from the rendering thread for each set of views that might be rendered.
+	* Game thread state like UObjects must have their properties mirrored on the proxy to avoid race conditions.  The rendering thread must not dereference UObjects.
+	* The gathered mesh elements will be used multiple times, any memory referenced must last as long as the Collector (eg no stack memory should be referenced).
+	* This function should not modify the proxy but simply collect a description of things to render.  Updates to the proxy need to be pushed from game thread or external events.
+	*
+	* @param Views - the array of views to consider.  These may not exist in the ViewFamily.
+	* @param ViewFamily - the view family, for convenience
+	* @param VisibilityMap - a bit representing this proxy's visibility in the Views array
+	* @param Collector - gathers the mesh elements to be rendered and provides mechanisms for temporary allocations
+	*/
 	virtual void GetDynamicMeshElements(const TArray<const FSceneView*>& Views, const FSceneViewFamily& ViewFamily, uint32 VisibilityMap, FMeshElementCollector& Collector) const override
 	{
+		//用于定义一个cpu时间统计的方法，详细可参考https://blog.csdn.net/leonwei/article/details/100099096
 		QUICK_SCOPE_CYCLE_COUNTER(STAT_FRayLineMeshSceneProxy_GetDynamicMeshElements);
 
+		//wireframe 线框模式
 		const bool bWireframe = AllowDebugViewmodes() && ViewFamily.EngineShowFlags.Wireframe;
 
 		auto WireframeMaterialInstance = new FColoredMaterialRenderProxy(
@@ -186,9 +304,11 @@ public:
 			FLinearColor(0, 0.5f, 1.f)
 		);
 
+		/** Add a material render proxy that will be cleaned up automatically */
 		Collector.RegisterOneFrameMaterialProxy(WireframeMaterialInstance);
 
 		FMaterialRenderProxy* MaterialProxy = NULL;
+		//判断绘制的是否是线框
 		if (bWireframe)
 		{
 			MaterialProxy = WireframeMaterialInstance;
@@ -198,8 +318,10 @@ public:
 			MaterialProxy = Material->GetRenderProxy(IsSelected());
 		}
 
+		//将模型数据加入到Collector
 		for (int32 ViewIndex = 0; ViewIndex < Views.Num(); ViewIndex++)
 		{
+			//因为需要绘制所有视图，所有遍历所有存在的视图
 			if (VisibilityMap & (1 << ViewIndex))
 			{
 				const FSceneView* View = Views[ViewIndex];
@@ -214,9 +336,9 @@ public:
 				BatchElement.FirstIndex = 0;
 				BatchElement.NumPrimitives = GetRequiredIndexCount() / 3;
 				BatchElement.MinVertexIndex = 0;
-				BatchElement.MaxVertexIndex = GetRequiredVertexCount();
+				BatchElement.MaxVertexIndex = GetRequiredVertexCount(); //这里为什么是GetRequiredVertexCount()，不是GetRequiredVertexCount()-1？
 				Mesh.ReverseCulling = IsLocalToWorldDeterminantNegative();
-				Mesh.Type = PT_TriangleList;
+				Mesh.Type = PT_TriangleList; //注意这个使用的是TriangleList，所以没三个顶点组成一个面。对应的NumPrimitives要设置对
 				Mesh.DepthPriorityGroup = SDPG_World;
 				Mesh.bCanApplyViewModeOverrides = false;
 				Collector.AddMesh(ViewIndex, Mesh);
@@ -224,6 +346,13 @@ public:
 		}
 	}
 
+	/**
+	* Determines the relevance of this primitive's elements to the given view.
+	* Called in the rendering thread.
+	* @param View - The view to determine relevance for.
+	* @return The relevance of the primitive's elements to the view.
+	*/
+	//确定绘制时需要哪些视图（可见）
 	virtual FPrimitiveViewRelevance GetViewRelevance(const FSceneView* View) const override
 	{
 		FPrimitiveViewRelevance Result;
@@ -237,30 +366,42 @@ public:
 		return Result;
 	}
 
+	/**
+	* @return true if the proxy can be culled when occluded by other primitives
+	*/
 	virtual bool CanBeOccluded() const override
 	{
 		return !MaterialRelevance.bDisableDepthTest;
 	}
 
+	/** Every derived class should override these functions */
 	virtual uint32 GetMemoryFootprint(void) const override { return(sizeof(*this) + GetAllocatedSize()); }
-
 	uint32 GetAllocatedSize(void) const { return(FPrimitiveSceneProxy::GetAllocatedSize()); }
 
 private:
 
-	UMaterialInterface * Material;
-	FStaticMeshVertexBuffers VertexBuffers;
+	UMaterialInterface* Material;
+	// FRayLineMeshVertexBuffer VertexBuffer; 4.17版本
+	FStaticMeshVertexBuffers VertexBuffers; //4.19版本
 	FRayLineMeshIndexBuffer IndexBuffer;
+	// FCustomMeshVertexFactory VertexFactory; 4.17版本
+	
+	//一个顶点工厂，仅将显式顶点属性从本地空间转换为世界空间
 	FLocalVertexFactory VertexFactory;
+
+	FRayLineDynamicData* DynamicData;
 
 	FMaterialRelevance MaterialRelevance;
 };
 
 //////////////////////////////////////////////////////////////////////////
+///
+//URayBasicComponent 相当于是逻辑线程
 
 URayBasicComponent::URayBasicComponent(const FObjectInitializer& ObjectInitializer)
 	: Super(ObjectInitializer)
 {
+	//一定要把三个开关打开，这样才能调用ComponentTick这些函数来更新我们的组件。
 	PrimaryComponentTick.bCanEverTick = true;
 	bTickInEditor = true;
 	bAutoActivate = true;
@@ -269,63 +410,57 @@ URayBasicComponent::URayBasicComponent(const FObjectInitializer& ObjectInitializ
 
 }
 
+//用于注册组件
 void URayBasicComponent::OnRegister()
 {
 	Super::OnRegister();
 
 	RayLineHitPoints.Reset();
-	RayLineHitPoints.AddUninitialized(10);
+	FVector RayDirection = FVector(1.0f, 0.0f, 0.0f);
+	FVector RayOrigin = FVector(0.0f, 0.0f, 0.0f);
+	int32 HitPointsNum = 10;
+	float SecLength = 50.0f;
 
+	RayLineHitPoints.AddUninitialized(HitPointsNum);
+	RayLineHitPoints[0].HitPosition = RayOrigin;
+	RayLineHitPoints[0].HitNextDir = RayDirection;
+
+	float t = DebugSec;
+	for (int32 i = 1; i < HitPointsNum; i++)
+	{
+		RayLineHitPoints[i].HitPosition = RayDirection * t + RayOrigin;
+
+		t += DebugSec;
+	}
+
+	//这个函数会开启一个开关，让引擎每帧更新所有组件渲染状态的时候，会更新到我们的组件。
 	MarkRenderDynamicDataDirty();
-}
-
-bool URayBasicComponent::RayTracingHit(FVector RayOrigin, FVector RayDirection, float RayMarhingLength, FHitResult& OutHitResoult)
-{
-	const TArray<AActor*>IgActor;
-	FVector startpos = RayOrigin;
-	FVector endpos = RayOrigin + RayDirection * RayMarhingLength;
-	return UKismetSystemLibrary::LineTraceSingle(
-		GetOwner(),
-		startpos,
-		endpos,
-		ETraceTypeQuery::TraceTypeQuery1,
-		false,
-		IgActor,
-		EDrawDebugTrace::Type::ForOneFrame,
-		OutHitResoult,
-		true,
-		FLinearColor::Blue,
-		FLinearColor::Red,
-		1.0f);
 }
 
 void URayBasicComponent::TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	//RayLineHitPoints.Reset();
+	RayLineHitPoints.Reset();
 	FVector RayDirection = FVector(1.0f, 0.0f, 0.0f);
-	FVector RayOrigin = GetOwner()->GetActorLocation();
+	FVector RayOrigin = FVector(0.0f, 0.0f, 0.0f);
 	int32 HitPointsNum = 10;
-	FHitResult OutHit;
+	float SecLength = 50.0f;
 
-	RayLineHitPoints[0].HitPosition = GetOwner()->GetTransform().Inverse().TransformPosition(RayOrigin);
+	RayLineHitPoints.AddUninitialized(HitPointsNum);
+	RayLineHitPoints[0].HitPosition = RayOrigin;
+	RayLineHitPoints[0].HitNextDir = RayDirection;
+
+	float t = DebugSec;
 	for (int32 i = 1; i < HitPointsNum; i++)
 	{
-		bool bHit = RayTracingHit(RayOrigin, RayDirection, 2000.0f, OutHit);
-		if (!bHit)
-		{
-			break;
-		}
-		FVector HitPointLoc = OutHit.Location + OutHit.ImpactNormal * 0.1f;
-		FVector HitPointLocTransformed = GetOwner()->GetTransform().Inverse().TransformPosition(HitPointLoc);
-		RayLineHitPoints[i].HitPosition = HitPointLocTransformed;
+		RayLineHitPoints[i].HitPosition = RayDirection * t + RayOrigin;
 
-		RayOrigin = HitPointLoc;
-		RayDirection = FMath::GetReflectionVector(RayDirection, OutHit.ImpactNormal);
+		t += DebugSec;
 	}
 
 	// Need to send new data to render thread
+	// 函数如果被调用，为组件开启了渲染状态开关，那么引擎就会自己调用CreateRenderState_Concurrent
 	MarkRenderDynamicDataDirty();
 
 	UpdateComponentToWorld();
@@ -343,10 +478,10 @@ void URayBasicComponent::SendRenderDynamicData_Concurrent()
 	if (SceneProxy)
 	{
 		FRayLineDynamicData* NewDynamicData = new FRayLineDynamicData;
-		NewDynamicData->HitpointsPosition.AddUninitialized(RayLineHitPoints.Num());
+		NewDynamicData->HitPointsPosition.AddUninitialized(RayLineHitPoints.Num());
 		for (int32 i = 0; i < RayLineHitPoints.Num(); i++)
 		{
-			NewDynamicData->HitpointsPosition[i] = RayLineHitPoints[i].HitPosition;
+			NewDynamicData->HitPointsPosition[i] = RayLineHitPoints[i].HitPosition;
 		}
 
 		// Enqueue command to send to render thread
@@ -360,16 +495,19 @@ void URayBasicComponent::SendRenderDynamicData_Concurrent()
 	}
 }
 
+//创建场景代理，这样逻辑线程中就会把数据传入到渲染线程中的这个场景代理中
 FPrimitiveSceneProxy* URayBasicComponent::CreateSceneProxy()
 {
 	return new FRayLineMeshSceneProxy(this);
 }
 
+//获取材质
 int32 URayBasicComponent::GetNumMaterials() const
 {
 	return 1;
 }
 
+//构建包围盒的操作了
 FBoxSphereBounds URayBasicComponent::CalcBounds(const FTransform& LocalToWorld) const
 {
 	FBoxSphereBounds NewBounds;
--- PrimitiveGeneration/Plugins/RayLine/Source/RayLine/Public/RayBasicComponent.h
+++ PrimitiveGeneration/Plugins/RayLine/Source/RayLine/Public/RayBasicComponent.h
@@ -4,18 +4,21 @@
 
 #include "CoreMinimal.h"
 #include "UObject/ObjectMacros.h"
+#include "Engine/EngineTypes.h"
 #include "Components/MeshComponent.h"
 #include "RayBasicComponent.generated.h"
 
+//这里前置先声明了FPrimitiveSceneProxy这个类，这个类是场景代理，负责渲染线程和逻辑线程的交互，它会将顶点缓冲，索引缓冲数据提交。
 class FPrimitiveSceneProxy;
 
+// 创建一个射线组件的基本数据结构
 USTRUCT(BlueprintType)
 struct FRayLineHitPointDesc
 {
 	GENERATED_USTRUCT_BODY()
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = RayLineHitPoint)
-	FVector HitPosition;
+		FVector HitPosition;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = RayLineHitPoint)
 		FVector HitNextDir;
@@ -25,21 +28,24 @@ struct FRayLineHitPointDesc
 };
 
 /** Component that allows you to specify custom triangle mesh geometry */
-UCLASS(hidecategories = (Object, LOD, Physics, Collision), editinlinenew, meta = (BlueprintSpawnableComponent), ClassGroup = Rendering, DisplayName = "RayBasicComponent")
+UCLASS(hidecategories = (Object, LOD, Physics, Collision, "Components|Collision"), editinlinenew, meta = (BlueprintSpawnableComponent), ClassGroup = Rendering)
 class URayBasicComponent : public UMeshComponent
 {
-	GENERATED_BODY()
+	//GENERATED_UCLASS_BODY() 用于生成默认构造函数以及相关配置
+	//GENERATED_BODY() 只生成相关配置，默认构造函数需自己写
 
-	URayBasicComponent(const FObjectInitializer& ObjectInitializer);
+	GENERATED_BODY()	
 
 public:
-
+	URayBasicComponent(const FObjectInitializer & ObjectInitializer);
+	
+	//UPROPERTY宏,暴露给蓝图并纳入垃圾回收中
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = RayLineComponent)
 		float DebugSec;
-
-private:
-
+	
 	//~ Begin UPrimitiveComponent Interface.
+	//会创建场景代理
+	//	场景代理的作用就是负责在渲染线程端把逻辑线程这边的数据压入渲染管线
 	virtual FPrimitiveSceneProxy* CreateSceneProxy() override;
 	//~ End UPrimitiveComponent Interface.
 
@@ -51,16 +57,19 @@ private:
 	virtual FBoxSphereBounds CalcBounds(const FTransform& LocalToWorld) const override;
 	//~ Begin USceneComponent Interface.
 
+	
+
 	//~ Begin UActorComponent Interface.
+	//这个函数负责组件的注册，会在组件构建的时候调用。
 	virtual void OnRegister() override;
+	//这个函数会每帧都调用。
 	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction) override;
+	//将逻辑线程的顶点数据发送到渲染线程
 	virtual void SendRenderDynamicData_Concurrent() override;
 	virtual void CreateRenderState_Concurrent() override;
 	//~ End UActorComponent Interface.
 
 	TArray<FRayLineHitPointDesc> RayLineHitPoints;
-
-	bool RayTracingHit(FVector RayOrigin, FVector RayDirection, float RayMarhingLength, FHitResult& OutHitResoult);
 
 	friend class FRayLineMeshSceneProxy;
 };
